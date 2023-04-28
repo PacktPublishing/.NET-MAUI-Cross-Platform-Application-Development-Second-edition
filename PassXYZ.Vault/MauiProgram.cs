9d5e23023a4ed502f0837dd1987e042185eae24a
Roger Ye <shugaoye@yahoo.com>
2023-04-28 16:58:50 +0800

    Build from V0.0.0 using .NET 7 and Visual Studio 17.5.0

---
+++ PassXYZ.Vault/MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using BlazorBindings.Maui;
+
+namespace PassXYZ.Vault;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<BlazorBindingsApplication<AppShell>>()
+            .UseMauiBlazorBindings()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}

0ca2500a81a901ce96f1ea38f57332ef44d72aa1
Roger Ye <shugaoye@yahoo.com>
2023-05-07 16:16:42 +0800

    Added Models, Services and Pages

--- PassXYZ.Vault/MauiProgram.cs
+++ PassXYZ.Vault/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using BlazorBindings.Maui;
+using PassXYZ.Vault.Models;
+using PassXYZ.Vault.Services;
 
 namespace PassXYZ.Vault;
 
@@ -13,14 +15,19 @@ public static class MauiProgram
             .UseMauiBlazorBindings()
             .ConfigureFonts(fonts =>
             {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("fa-regular-400.ttf", "FontAwesomeRegular");
+				fonts.AddFont("fa-solid-900.ttf", "FontAwesomeSolid");
+				fonts.AddFont("fa-brands-400.ttf", "FontAwesomeBrands");
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
 #if DEBUG
 		builder.Logging.AddDebug();
+		builder.Logging.SetMinimumLevel(LogLevel.Debug);
 #endif
+		builder.Services.AddSingleton<IDataStore<Item>, MockDataStore>();
 
-        return builder.Build();
+		return builder.Build();
     }
 }
---
+++ PassXYZ.Vault/Services/MockDataStore.cs
@@ -0,0 +1,78 @@
+﻿using PassXYZ.Vault.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PassXYZ.Vault.Services
+{
+    public class MockDataStore : IDataStore<Item>
+    {
+        readonly List<Item> items;
+
+        public MockDataStore()
+        {
+            items = new List<Item>()
+            {
+                new Item { Id = Guid.NewGuid().ToString(), Name = "First item", Description="This is an item description." },
+                new Item { Id = Guid.NewGuid().ToString(), Name = "Second item", Description="This is an item description." },
+                new Item { Id = Guid.NewGuid().ToString(), Name = "Third item", Description="This is an item description." },
+                new Item { Id = Guid.NewGuid().ToString(), Name = "Fourth item", Description="This is an item description." },
+                new Item { Id = Guid.NewGuid().ToString(), Name = "Fifth item", Description="This is an item description." },
+                new Item { Id = Guid.NewGuid().ToString(), Name = "Sixth item", Description="This is an item description." }
+            };
+        }
+
+        public async Task<bool> AddItemAsync(Item item)
+        {
+            if (item == null) { throw new ArgumentNullException(nameof(item)); }
+            items.Add(item);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(Item item)
+        {
+            if (item == null) { throw new ArgumentNullException(nameof(item)); }
+
+            var oldItem = items.Where((Item arg) => arg.Id == item.Id).FirstOrDefault();
+            if (oldItem != null) 
+            {
+                items.Remove(oldItem);
+                items.Add(item);
+                return await Task.FromResult(true);
+            }
+            else
+            {
+                return await Task.FromResult(false);
+            }
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            if (id == null) { throw new ArgumentNullException(nameof(id)); }
+
+            var oldItem = items.Where((Item arg) => arg.Id == id).FirstOrDefault();
+            if (oldItem != null) 
+            {
+                items.Remove(oldItem);
+                return await Task.FromResult(true);
+            }
+            else
+            {
+                return await Task.FromResult(false);
+            }
+        }
+
+        public async Task<Item?> GetItemAsync(string id)
+        {
+            if(id == null) { throw new ArgumentNullException(nameof(id)); }
+
+            return await Task.FromResult(items.FirstOrDefault(s => s.Id == id));
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
+}

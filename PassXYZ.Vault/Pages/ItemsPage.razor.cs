3ab666f63a7a1cd12e08ce647e127ce31c489b77
Roger Ye <shugaoye@yahoo.com>
2023-05-13 10:32:34 +0800

    Support NewItemPage

--- PassXYZ.Vault/Pages/ItemsPage.razor.cs
+++ PassXYZ.Vault/Pages/ItemsPage.razor.cs
@@ -2,8 +2,10 @@ using System.Collections.ObjectModel;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.Logging;
+using MC = Microsoft.Maui.Controls;
 using PassXYZ.Vault.Models;
 using PassXYZ.Vault.Services;
+using BlazorBindings.Maui;
 
 namespace PassXYZ.Vault.Pages
 {
@@ -25,6 +27,6 @@ namespace PassXYZ.Vault.Pages
             Items = await dataStore.GetItemsAsync(true);
             StateHasChanged();
         }
-    }
 
+    }
 }
